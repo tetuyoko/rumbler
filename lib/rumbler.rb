25104e5422530045fbbe92a9d0b0a906f3916268
tetuyoko <tyokoyama53@gmail.com>
2012-04-18 00:37:35 +0900

    実行ファイルを追加

--- lib/rumbler.rb
+++ lib/rumbler.rb
@@ -1,7 +1,32 @@
-#!/usr/bin/env ruby
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 require 'progressbar'
 require 'rumbler/shuffle'
 
-Rumbler::Shuffle.start
+module Rumbler
+  class << self
+    def play(amount=100)
+      shuffle = Shuffle.new(amount, score_board)
+      shuffle.rumble
+      finish 
+    end
+
+    def score_board
+      @score_board ||= create_score_board
+    end
+
+    def create_score_board
+      keys = <<-EOF.gsub(/^\s*/,'').split("\n")
+        Tetuyoko
+        Yokoyama
+        HogeR
+      EOF
+      values = Array.new(keys.size, 0)
+      Hash[keys.zip(values)]
+    end
+
+    def finish
+      winner = score_board.max_by {|k,v| v }.first
+      puts "#{winner} is got a prize!"
+    end
+  end
+end
 
--- lib/rumbler/shuffle.rb
+++ lib/rumbler/shuffle.rb
@@ -1,32 +1,24 @@
 module Rumbler
   class Shuffle
-    def self.start
-      amount = 100
+    def initialize(amount, score_board) 
+      @amount, @score_board = amount, score_board
+      @progs = @score_board.keys.map {|key| ProgressBar.new(key, @amount) }
+    end
 
-      keys = <<-EOF.gsub(/^\s*/,'').split("\n")
-        Tetuyoko
-        Yokoyama
-        HogeR
-      EOF
-
-      values = Array.new(keys.size, 0)
-      score_board = Hash[keys.zip(values)]
-
-      amount.times { score_board[keys.sample] += 1 }
-
-      @progs = keys.map {|key| ProgressBar.new(key, amount) }
-
-      score_board.each_pair do |key, value|
+    def rumble
+      @amount.times { up }
+      @score_board.each_pair do |key, value|
         active_bar = pbar(key)
         value.times { sleep 0.01; active_bar.inc}
         active_bar.halt
       end
-
-      winner = score_board.max_by {|k,v| v }.first
-      puts "#{winner} is got a prize!"
     end
 
-    def self.pbar(key)
+    def up
+      @score_board[@score_board.keys.sample] += 1
+    end
+
+    def pbar(key)
       @progs.detect {|n| n.title == key }
     end
   end
